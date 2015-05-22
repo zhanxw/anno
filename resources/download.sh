e997468143f50505dabc4d277a48c7c97346a5e7
zhanxw <zhanxw@users.noreply.github.com>
2015-05-22 01:51:15 -0400

    Update download.sh

--- resources/download.sh
+++ resources/download.sh
@@ -3,15 +3,15 @@ echo "Begin download TabAnno resource files under current directory..."
 echo 
 
 echo "Download reference file and its index:"
-wget http://www.sph.umich.edu/csg/zhanxw/software/anno/resources/hs37d5.fa
-wget http://www.sph.umich.edu/csg/zhanxw/software/anno/resources/hs37d5.fa.fai
+wget http://qbrc.swmed.edu/zhanxw/software/anno/resources/hs37d5.fa
+wget http://qbrc.swmed.edu/zhanxw/software/anno/resources/hs37d5.fa.fai
 
 echo "Download gene definition:"
-wget http://www.sph.umich.edu/csg/zhanxw/software/anno/resources/refFlat_hg19.txt.gz
+wget http://qbrc.swmed.edu/zhanxw/software/anno/resources/refFlat_hg19.txt.gz
 
 echo "Download TabAnno codon definition and annotation priority files:"
-wget http://www.sph.umich.edu/csg/zhanxw/software/anno/codon.txt
-wget http://www.sph.umich.edu/csg/zhanxw/software/anno/priority.txt
+wget http://qbrc.swmed.edu/zhanxw/software/anno/codon.txt
+wget http://qbrc.swmed.edu/zhanxw/software/anno/priority.txt
 
 echo "Download finished."
 echo 
